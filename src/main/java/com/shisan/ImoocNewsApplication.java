55fffe2bccef7c8fad69167a484272a89100a284
shisan <13502151671@163.com>
2023-03-06 22:14:19 +0800

    构建模块工程

---
+++ src/main/java/com/shisan/ImoocNewsApplication.java
@@ -0,0 +1,13 @@
+package com.shisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shisan/ImoocNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shisan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
